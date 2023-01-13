71a55f9b55829ceae42f9e15939b71d84da0cba0
tannybrown <121401159+tannybrown@users.noreply.github.com>
2023-01-13 18:57:47 +0900

    Create 1. Two Sum.py

---
+++ 1. Two Sum.py
@@ -0,0 +1,16 @@
+#첫시도
+class Solution(object):
+    def twoSum(self, nums, target):
+        arr = []
+        for i in range(len(nums)):
+            j = len(nums) -1
+            while j > i:
+                value = nums[i] + nums[j]
+                arr.append([value,i,j])
+                j-=1
+
+        for i in arr:
+            if i[0] == target:
+                return [i[1],i[2]]
+        
+#두번째 시도
