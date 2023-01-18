18462914d82ab0365671540f23e2244c748fe12f
tannybrown <121401159+tannybrown@users.noreply.github.com>
2023-01-18 18:32:16 +0900

    Update 1470. Shuffle the Array.py

--- 1470. Shuffle the Array.py
+++ 1470. Shuffle the Array.py
@@ -4,3 +4,12 @@ class Solution(object):
             nums.append(nums[i])
             nums.append(nums[n+i])
         return nums[2*n:]
+    
+# 다른 방법
+class Solution(object):
+    def shuffle(self, nums, n):
+        ans = []
+        for i in range(n):
+            ans.append(nums[i])
+            ans.append(nums[n+i])
+        return ans
